98c756f42324ef289c779566a57d1bfa2f0c1083
MehreenZahra <codewithmehreen1@gmail.com>
2024-08-12 19:57:01 +0500

    msg notification icons removed

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -62,12 +62,12 @@ export default function Navbar() {
         <Diversity2Icon sx={{display:{xs:"block", sm:"none"}}}/>
         <Search> <InputBase placeholder='Search...'/></Search>
         <Icons  sx={{ "&:hover": {cursor: 'pointer' }}}>
-        <Badge badgeContent={4} color="error">
+        {/* <Badge badgeContent={4} color="error">
         <MailIcon />
         </Badge>
         <Badge badgeContent={6} color="error">
         <NotificationsIcon />
-       </Badge>
+       </Badge> */}
       {avatarUrl ? (<Avatar src={avatarUrl}/>) : ( <Avatar sx={{width:40, height:40}} >{initials}</Avatar>)}
       {/* src="https://images.pexels.com/photos/8575841/pexels-photo-8575841.jpeg"  */}
        {profile && <Typography className="font-semibold" variant='span'>{profile.firstName}</Typography>}
